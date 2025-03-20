4454ed86eeee0f9f558c5957eeac06cc1feea065
sowbaranigakps-04 <sowbaranigakps04@gmail.com>
2025-03-20 14:46:05 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u ar8888 -p dckr_pat_HU8f1bsQJp3j2DnCttXuuoppo7w
-    docker tag test ar8888/dev
-    docker push ar8888/dev
-    docker-compose up -d
+    docker login -u sowbaranigak -p dckr_pat_rX9vAMG0tgy921MUInYbZ3Hrdts
+    docker tag test sowbaranigak/dev
+    docker push sowbaranigak/dev
+  
     
